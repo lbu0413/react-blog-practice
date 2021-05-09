c4a5a851f3ea01dda99f711d1cbb0f6db3f99e44
LBU <noeliden1@gmail.com>
2021-05-09 05:55:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState(['front-end frameworks', 'React', 'Vue']);
+  const [like, setLike] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        Wook's blog
+      </div>
+      <div className="list">
+        <h3>{ title[0] } 
+          <span onClick={() => { setLike(like + 1) }}>👍</span>{ like }
+        </h3>
+        <p>Feb. 17th</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ title[1] } </h3>
+        <p>Feb. 17th</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ title[2] } </h3>
+        <p>Feb. 17th</p>
+        <hr/>
+      </div>
     </div>
   );
 }
